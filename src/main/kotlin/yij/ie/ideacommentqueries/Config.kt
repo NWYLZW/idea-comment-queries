e5cc3174aebde646355d9337e2580997e4954840
yijie4188 <yijie4188@gmail.com>
2023-10-21 00:14:29 +0800

    feat: support disable plugin in current project

--- src/main/kotlin/yij/ie/ideacommentqueries/Config.kt
+++ src/main/kotlin/yij/ie/ideacommentqueries/Config.kt
@@ -1,22 +1,36 @@
 package yij.ie.ideacommentqueries
 
 import com.intellij.openapi.options.Configurable
-import javax.swing.JComponent
+import com.intellij.openapi.project.Project
+import com.intellij.ui.components.JBLabel
+import com.intellij.util.ui.FormBuilder
+import javax.swing.JCheckBox
 
-class Config : Configurable {
-    override fun createComponent(): JComponent? {
-        TODO("Not yet implemented")
-    }
+open class Config(private val project: Project) : Configurable {
+    override fun getDisplayName() = "Comment Queries"
+
+    private val service = ConfigService.getInstance(project)
+
+    private val disableCheckBox = JCheckBox("Disable for this project")
+    private val panel = FormBuilder
+        .createFormBuilder()
+        .addComponent(disableCheckBox, 1)
+        .addComponentFillVertically(JBLabel(""), 0)
+        .panel
+
+    override fun createComponent() = panel!!
 
     override fun isModified(): Boolean {
-        TODO("Not yet implemented")
+        var modified = false
+        modified = modified || disableCheckBox.isSelected != service.disable
+        return modified
     }
 
     override fun apply() {
-        TODO("Not yet implemented")
+        service.disable = disableCheckBox.isSelected
     }
 
-    override fun getDisplayName(): String {
-        TODO("Not yet implemented")
+    override fun reset() {
+        disableCheckBox.isSelected = service.disable
     }
 }
---
+++ src/main/kotlin/yij/ie/ideacommentqueries/ConfigService.kt
@@ -0,0 +1,26 @@
+package yij.ie.ideacommentqueries
+
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.project.Project
+import com.intellij.util.xmlb.XmlSerializerUtil
+
+@Service(Service.Level.PROJECT)
+@State(name = "CommentQueriesConfigService", storages = [Storage("comment-queries.xml")])
+class ConfigService : PersistentStateComponent<ConfigService> {
+    var disable: Boolean = false
+    override fun getState(): ConfigService {
+        return this
+    }
+    override fun loadState(config: ConfigService) {
+        XmlSerializerUtil.copyBean(config, this)
+    }
+
+    companion object {
+        fun getInstance(project: Project): ConfigService {
+            return project.getService(ConfigService::class.java)
+        }
+    }
+}
