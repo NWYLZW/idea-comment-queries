7f1c95bbca08d1383246844b12a03150119badaa
yijie4188 <yijie4188@gmail.com>
2023-10-20 16:57:29 +0800

    feat: add plugin Configurable

---
+++ src/main/kotlin/yij/ie/ideacommentqueries/Config.kt
@@ -0,0 +1,22 @@
+package yij.ie.ideacommentqueries
+
+import com.intellij.openapi.options.Configurable
+import javax.swing.JComponent
+
+class Config : Configurable {
+    override fun createComponent(): JComponent? {
+        TODO("Not yet implemented")
+    }
+
+    override fun isModified(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun apply() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDisplayName(): String {
+        TODO("Not yet implemented")
+    }
+}
