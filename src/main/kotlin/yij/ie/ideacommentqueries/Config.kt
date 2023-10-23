a169eed3ab02cf09f373f13bb90a049053e8de16
yijie4188 <yijie4188@gmail.com>
2023-10-24 00:59:50 +0800

    feat: add tooltip for disable

--- src/main/kotlin/yij/ie/ideacommentqueries/Config.kt
+++ src/main/kotlin/yij/ie/ideacommentqueries/Config.kt
@@ -4,6 +4,8 @@ import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
 import com.intellij.ui.components.JBLabel
 import com.intellij.util.ui.FormBuilder
+import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.UIUtil
 import javax.swing.JCheckBox
 
 open class Config(private val project: Project) : Configurable {
@@ -15,6 +17,20 @@ open class Config(private val project: Project) : Configurable {
     private val panel = FormBuilder
         .createFormBuilder()
         .addComponent(disableCheckBox, 1)
+        .addComponentToRightColumn(object : JBLabel(
+            """
+            Disable comment queries for this project, and you can use `/* comment-queries-enable */` to enable it for a file.
+            """.trimIndent(), UIUtil.ComponentStyle.SMALL, UIUtil.FontColor.BRIGHTER
+        ) {
+            init { border = JBUI.Borders.emptyLeft(13) }
+        }, 0)
+        .addComponentToRightColumn(object : JBLabel(
+            """
+            If you want to disable it for a file, you can use `/* comment-queries-disable */`.
+            """.trimIndent(), UIUtil.ComponentStyle.SMALL, UIUtil.FontColor.BRIGHTER
+        ) {
+            init { border = JBUI.Borders.emptyLeft(13) }
+        }, 0)
         .addComponentFillVertically(JBLabel(""), 0)
         .panel
 
